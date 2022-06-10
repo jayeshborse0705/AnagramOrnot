3cd81c28f67068d806f2b3cb9801d6cdbd934b17
jayeshborse0705 <jayeshborse0705@gmail.com>
2022-06-10 09:46:04 +0530

    [jayesh]  given two string anagram or not

---
+++ AnagramOrnot1/Check.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace AnagramOrnot1
+{
+    internal class Check
+    {
+        public void angramorno()
+        {
+            Console.WriteLine(" Enter First string :");
+            string firststr = Console.ReadLine();
+            Console.WriteLine(" Enter Second  string");
+            string secondstr = Console.ReadLine();
+            if (firststr.Length == secondstr.Length)
+            {
+                char[] ch1 = (firststr.ToLower()).ToCharArray();
+                char[] ch2 = (firststr.ToLower()).ToCharArray();
+                Array.Sort(ch1);
+                Array.Sort(ch2);
+                string str1 = new string(ch1);
+                string str2 = new string(ch2);
+                if (str1==str2)
+                {
+                    Console.WriteLine("Anagram given two string");
+                }
+                else
+                {
+                    Console.WriteLine(" Not Anagram given string ");
+                }
+
+
+
+
+
+            }
+
+
+        }
+    }
+}
---
+++ AnagramOrnot1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace AnagramOrNot
+{
+    public class progran
+    {
+        public static void Main(String[] args)
+        {
+            Check chec = new Check();
+            chec.angramorno();
+        }
+    }
+}
